4e8ca8985371f19229613ce7b7ab4726c240b0b8
Oathbound <latiosgigetto@gmail.com>
2024-07-02 17:53:23 +0200

    Rudimentary gameMaster implementation

---
+++ controller/gameMaster.ts
@@ -0,0 +1,102 @@
+/**
+ *  The GameMaster file is responsible for managing the game state and
+ *  handling all game logic
+ * 
+ */
+import { DBAccess } from '../db-connection/database';
+import { Request, Response } from 'express';
+import { Sequelize } from 'sequelize';
+import { User } as UserModel from '../model/User';
+import { Game } as UserModel from '../model/Game';
+
+
+
+const connection: Sequelize = DBAccess.getInstance();
+
+/**
+ * 
+ *  Function that creates a new game instance.
+ * 
+ * @param req 
+ * @param res 
+ */
+
+export async function newGame(req: Request, res: Response): Promise<void> {
+
+    // default game state
+
+    var gameState = ['', '', '', '', '', '', '', '', ''];
+
+    try {
+        if (req.body.gameMode == '2D') {
+            var gameState = ['', '', '', '', '', '', '', '', ''];
+        }/** else {
+        var gameState = ['', '', '', '', '', '', '', '',
+                         '', '', '', '', '', '', '', '',
+                         '', '', '', '', '', '', '', '',
+                         '', '', '', '', '', '', '', '',
+                         '', '', '', '', '', '', '', '',
+                         '', '', '', '', '', '', '', '',
+                         '', '', '', '', '', '', '', '',
+                         '', '', '', '', '', '', '', '',];
+    } */
+
+        await GameModel.Game.create({
+            gameMode: req.body.gameMode,
+            startTime: new Date(),
+            player1: req.body.playerOne,
+            player2: req.body.gameOpponent,
+            status: 'IN PROGRESS',
+            turnTime: req.body.turnTime,
+            currentTurn: req.body.playerOne,
+            gameState: gameState,
+            winner: 'TBD',
+        }).then((game: any) => {
+
+            UserModel.User.update({ inGame: true }, { where: { email: req.body.playerOne } });
+
+            if (req.body.gameOpponent != 'AI') {
+                UserModel.User.update({ inGame: true }, { where: { email: req.body.gameOpponent } });
+            }
+
+            // TODO: find a way to implement a more complex message handler
+
+            const response = { message: 'Game created successfully', status: 200 };
+            res.header('Content-Type', 'application/json');
+            res.status(response.status).json({ Message: response.message, ID: game.id });
+        });
+    } catch (error) {
+        console.log(error);
+        //  errorHandler(error, res);
+    }
+}
+
+/**
+ * 
+ * Function that retrieves the status of a given game by id.
+ * 
+ * @param req 
+ * @param res 
+ */
+
+export async function getGame(req: Request, res: Response): Promise<void> {
+    try {
+        const id = req.params.id;
+        await GameModel.Game.findOne({
+            attributes: ['status', 'player1', 'player2', 'currentTurn', 'gameState', 'winner'],
+            where: { gameId: id }
+        }).then((game: any) => {
+
+            // TODO: find a way to implement a more complex message handler
+
+            const response = { message: 'Game status retrieved successfully', status: 200 };
+            res.header('Content-Type', 'application/json');
+            res.status(response.status).json({ Message: response.message, GameStatus: game });
+        });
+    } catch (error) {
+        console.log(error);
+        // errorHandler(error, res);
+
+    }
+
+}
